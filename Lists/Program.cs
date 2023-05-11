5e75089be74c17e383218ea95886c3320ceaecec
Mohammad Jabir <mohammadj94@gmail.com>
2023-05-11 00:35:36 -0600

    code complete.

--- Lists/Program.cs
+++ Lists/Program.cs
@@ -1,2 +1,23 @@
 ﻿
+// string[] firstNames = new string[5]
 List<string> firstNames = new List<string>();
+
+firstNames.Add("Tim");
+firstNames.Add("Bob");
+firstNames.Add("Sue");
+firstNames.Add("Kane");
+firstNames.Add("Barney");
+
+//Console.WriteLine(firstNames[firstNames.Count - 1]);
+
+// List<T> - generic
+
+string data = "Corey,Smith,Jones";
+
+List<string> lastNames = data.Split(',').ToList();
+lastNames.Add("Franklin");
+
+foreach (string name in lastNames)
+{
+    Console.WriteLine(name);
+}
